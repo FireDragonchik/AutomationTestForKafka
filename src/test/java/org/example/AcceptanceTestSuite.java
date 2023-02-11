1fe0306e3f5b3fff48b2ca53f47eb8e8c6d96867
GDN\nikolaus.wijaya <nikolaus.wijaya@gdn-commerce.com>
2023-02-11 12:46:13 +0700

    initial commit

---
+++ src/test/java/org/example/AcceptanceTestSuite.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+    stepNotifications = true,
+    features="src/test/resources/features/",
+    glue="org.example")
+public class AcceptanceTestSuite {}
